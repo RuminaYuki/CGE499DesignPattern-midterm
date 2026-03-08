89186c4d571c21fa78fcfb09bddb6dd225525e7e
RuminaYuki <donminecraft36@gmail.com>
2026-03-08 09:15:36 +0700

    Final

--- SaveMyPriest/Assets/Script/Pattern/Singleton.cs
+++ SaveMyPriest/Assets/Script/Pattern/Singleton.cs
@@ -2,13 +2,12 @@ using UnityEngine;
 
 public class Singleton<T> : MonoBehaviour where T : Component
 {
-    private static T _instance;
     public static T Instance{get; private set; }
     protected virtual void Awake()
     {
-        if (_instance == null)
+        if (Instance == null)
         {
-            _instance = this as T;
+            Instance = this as T;
             DontDestroyOnLoad(gameObject);
         }
         else
